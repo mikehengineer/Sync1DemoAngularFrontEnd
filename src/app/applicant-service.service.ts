402c7823d1c3f7f212b1dec98bb9e82ba210f431
mikehengineer <mikehengineer@gmail.com>
2020-09-02 13:27:33 -0500

    Add files via upload

---
+++ src/app/applicant-service.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicantServiceService {
+  private headers: HttpHeaders;
+  private applicantsAPI = 'https://localhost:44382/api/applicants';
+ 
+
+  constructor(private http: HttpClient) { 
+    this.headers = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'});
+  }
+
+  public get() {
+    // Get all applicants and their data
+    return this.http.get(this.applicantsAPI, {headers: this.headers});
+  }
+
+  public add(data) {
+    return this.http.post(this.applicantsAPI, data, {headers: this.headers});
+  }
+
+  public remove(data) {
+    console.log("Inside remove, data.softDelete: ", data.softDelete);
+    return this.http.delete(this.applicantsAPI + '/' + data.id, {headers: this.headers});
+  }
+
+  public update(data) {
+    return this.http.put(this.applicantsAPI + '/' + data.id, data, {headers: this.headers});
+  }
+
+}
